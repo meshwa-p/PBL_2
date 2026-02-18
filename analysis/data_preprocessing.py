bd63de176c9dda9df089a98589d967af6356b851
Meshwa Sunil Patel <meshwapatel2005@gmail.com>
2026-02-18 23:38:55 +0530

    Update data_preprocessing.py

--- analysis/data_preprocessing.py
+++ analysis/data_preprocessing.py
@@ -2,14 +2,11 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 def load_and_preprocess(path):
-    # Load dataset
     df = pd.read_csv(path)
 
-    # Separate features and label
     X = df.drop("label", axis=1)
     y = df["label"]
 
-    # Scale features
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X)
 
